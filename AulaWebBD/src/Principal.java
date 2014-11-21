5e614355c8793a5381888cff3c72974776903049
Rodolpho Henrique <rodolpho@squarebits.com.br>
2014-11-20 22:15:10 -0200

    JavaWeb+ jpa+ Hibernate

--- AulaWebBD/src/Principal.java
+++ AulaWebBD/src/Principal.java
@@ -1,7 +1,10 @@
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 
 public class Principal {
@@ -19,9 +22,22 @@ public class Principal {
 		EntityManager em = emf.createEntityManager();
 		
 		EntityTransaction tx = em.getTransaction();
+		
 		tx.begin();
 		em.persist(p);
 		tx.commit();
+		
+		Query q = em.createQuery("SELECT p FROM Produto p");
+		List <Produto> produtos = q.getResultList();
+		
+		for(Produto prod: produtos){
+			System.out.println(prod.getNome());
+		}
+		
+		em.close();
+		emf.close();
+		
+		
 	}
 
 }
